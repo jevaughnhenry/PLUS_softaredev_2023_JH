59752724b6cf2f95cf8828c36f47db44e2a2b272
Henry Jevaughn <jevaughn.henry@stud.plus.ac.at>
2023-03-20 11:44:41 +0100

    edit of second

--- second_file.py
+++ second_file.py
@@ -1,12 +1,11 @@
 import os
 import sys
 
-print(
-sys.path
+print(sys.path)
 help("modules")
-days_of_the_week
-= 7
-def
-    say_hello recipient
+
+days_of_the_week = 7
+
+def say_hello recipient:
     print("Hello, world! Hello {} ".format(recipient))
     return recipient
